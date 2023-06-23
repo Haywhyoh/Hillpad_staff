7ffb08756a03f54b982d8bb17d0587219f7f465a
Martins Anerua <aneruamartins@gmail.com>
2023-06-23 10:26:00 +0100

    refactor: convert NavBar to class component

--- Hillpad_staff/src/components/NavBar.jsx
+++ Hillpad_staff/src/components/NavBar.jsx
@@ -1,11 +1,14 @@
+import React, { Component } from 'react';
+
 import avatar1 from '../assets/img/avatars/1.png';
 
 
-function NavBar() {
-
-    return (
-        <>
-            <nav className="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+class NavBar extends Component {
+    
+    render() { 
+        return (
+            <>
+                <nav className="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
                     id="layout-navbar">
                     <div className="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
                         <a className="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
@@ -94,8 +97,10 @@ function NavBar() {
                         </ul>
                     </div>
                 </nav>
-        </>
-    );
+            </>
+
+        );
+    }
 }
 
 export default NavBar;
