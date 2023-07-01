c1ca4bcab9f511345b538d4b23328e955e1cfd0d
Martins Anerua <aneruamartins@gmail.com>
2023-07-01 23:54:36 +0100

    feat: add school api service

--- Hillpad_staff/src/services/api/schoolService.js
+++ Hillpad_staff/src/services/api/schoolService.js
@@ -3,12 +3,68 @@ import config from "../../config.json";
 
 const apiEndPoint = `${config.apiURL}/academics/school`;
 
+// School routes
 function getSchools() {
     const url = `${apiEndPoint}/list`;
     return http.get(url);
 }
 
+function getSchool(id) {
+    const url = `${apiEndPoint}/detail/${id}`;
+    return http.get(url);
+}
+
+// School Draft routes
+function getSchoolDrafts() {
+    const url = `${apiEndPoint}/list_draft`;
+    return http.get(url);
+}
+
+function getSchoolDraft(id) {
+    const url = `${apiEndPoint}/detail_draft/${id}`;
+    return http.get(url);
+}
+
+function createSchoolDraft(data) {
+    const url = `${apiEndPoint}/create_draft`;
+    return http.post(url, data);
+}
+
+function updateSchoolDraft(id, data) {
+    const url = `${apiEndPoint}/update_draft/${id}`;
+    return http.patch(url, data);
+}
+
+function submitSchoolDraft(id, data) {
+    const url = `${apiEndPoint}/submit_draft/${id}`;
+    return http.patch(url, data);
+}
+
+function rejectSchoolDraft(id, data) {
+    const url = `${apiEndPoint}/reject_draft/${id}`;
+    return http.put(url, data);
+}
+
+function approveSchoolDraft(id) {
+    const url = `${apiEndPoint}/approve_draft/${id}`;
+    return http.put(url);
+}
+
+function publishSchoolDraft(id) {
+    const url = `${apiEndPoint}/publish_draft/${id}`;
+    return http.put(url);
+}
+
 
 export default {
     getSchools,
+    getSchool,
+    getSchoolDrafts,
+    getSchoolDraft,
+    createSchoolDraft,
+    updateSchoolDraft,
+    submitSchoolDraft,
+    rejectSchoolDraft,
+    approveSchoolDraft,
+    publishSchoolDraft
 }
