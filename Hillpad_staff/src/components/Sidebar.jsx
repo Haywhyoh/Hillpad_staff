e249e9fa52e7c593b84310cd67da77c7eadf0c70
Martins Anerua <aneruamartins@gmail.com>
2023-08-09 13:02:46 +0100

    feat: conditional rendering of actions menu

--- Hillpad_staff/src/components/Sidebar.jsx
+++ Hillpad_staff/src/components/Sidebar.jsx
@@ -1,78 +1,85 @@
-// eslint-disable-next-line no-unused-vars
-import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
-import hillpadLogo from '../assets/img/hillpad/logos/hillpad-transparent.png';
+import useAuth from "../hooks/useAuth";
 
 import MenuItem from './MenuItem.jsx';
 
+import hillpadLogo from '../assets/img/hillpad/logos/hillpad-transparent.png';
 
-class Sidebar extends Component {
+
+
+const Sidebar = () => {
+    const { user } = useAuth();
      
-    render() {
-        return (
-            <>
-                <aside id="layout-menu" className="layout-menu menu-vertical menu bg-menu-theme">
-                    <div className="app-brand demo">
-                        <Link to="/" className="app-brand-link">
-                            <img width="80%" src={ hillpadLogo } className="d-block" alt="HillPad logo" />
+    return (
+        <>
+            <aside id="layout-menu" className="layout-menu menu-vertical menu bg-menu-theme">
+                <div className="app-brand demo">
+                    <Link to="/" className="app-brand-link">
+                        <img width="80%" src={ hillpadLogo } className="d-block" alt="HillPad logo" />
+                    </Link>
+
+                    <Link to="/" className="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+                        <i className="bx bx-chevron-left bx-sm align-middle"></i>
+                    </Link>
+                </div>
+
+                <div className="menu-inner-shadow"></div>
+
+                <ul className="menu-inner py-1">
+                    
+                    {/* Dashboard */}
+                    <li id="menu-item-dashboard" className="menu-item mt-4">
+                        <Link to="/" className="menu-link">
+                            <i className="menu-icon tf-icons bx bx-home-circle"></i>
+                            <div data-i18n="Analytics">Dashboard</div>
                         </Link>
+                    </li>
 
-                        <Link to="/" className="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
-                            <i className="bx bx-chevron-left bx-sm align-middle"></i>
-                        </Link>
-                    </div>
+                    {/* Entries */}
+                    <li className="menu-header small text-uppercase">
+                        <span className="menu-header-text">Entries</span>
+                    </li>
+                    <MenuItem entryName="Courses" entryURL="/course" entryIcon="bx-book-open" />
+                    <MenuItem entryName="Schools" entryURL="/school" entryIcon="bxs-school" />
+                    <MenuItem entryName="Disciplines" entryURL="/discipline" entryIcon="bx-cabinet" />
+                    <MenuItem entryName="Degree types" entryURL="/degree-type" entryIcon="bxs-graduation" />
+                    <MenuItem entryName="Countries" entryURL="/country" entryIcon="bx-globe" />
 
-                    <div className="menu-inner-shadow"></div>
 
-                    <ul className="menu-inner py-1">
-                        
-                        {/* Dashboard */}
-                        <li id="menu-item-dashboard" className="menu-item mt-4">
-                            <Link to="/" className="menu-link">
-                                <i className="menu-icon tf-icons bx bx-home-circle"></i>
-                                <div data-i18n="Analytics">Dashboard</div>
-                            </Link>
-                        </li>
+                    {/* Actions */}
+                    {
+                        user.role &&
+                        user.role !== "SPECIALIST" &&
+                        <>
+                            <li className="menu-header small text-uppercase">
+                                <span className="menu-header-text">Actions</span>
+                            </li>
+                            <MenuItem entryName="Course Actions" badge="17" entryURL="/course-actions" entryIcon="bx-book-open" />
+                            <MenuItem entryName="School Actions" badge="6" entryURL="/school-actions" entryIcon="bxs-school" />
+                        </>
+                    }
 
-                        {/* Entries */}
-                        <li className="menu-header small text-uppercase">
-                            <span className="menu-header-text">Entries</span>
-                        </li>
-                        <MenuItem entryName="Courses" entryURL="/course" entryIcon="bx-book-open" />
-                        <MenuItem entryName="Schools" entryURL="/school" entryIcon="bxs-school" />
-                        <MenuItem entryName="Disciplines" entryURL="/discipline" entryIcon="bx-cabinet" />
-                        <MenuItem entryName="Degree types" entryURL="/degree-type" entryIcon="bxs-graduation" />
-                        <MenuItem entryName="Countries" entryURL="/country" entryIcon="bx-globe" />
+                    
+                    {/* Account */}
+                    <li className="menu-header small text-uppercase">
+                        <span className="menu-header-text">Account</span>
+                    </li>
+                    <MenuItem entryName="Notifications" entryURL="/notifications" entryIcon="bx-bell" />
+                    <MenuItem entryName="Chat" entryURL="/chat" entryIcon="bx-message-rounded-dots" />
 
-                        {/* Actions */}
-                        <li className="menu-header small text-uppercase">
-                            <span className="menu-header-text">Actions</span>
-                        </li>
-                        <MenuItem entryName="Course Actions" badge="17" entryURL="/course-actions" entryIcon="bx-book-open" />
-                        <MenuItem entryName="School Actions" badge="6" entryURL="/school-actions" entryIcon="bxs-school" />
+                    {/* Misc */}
+                    <li className="menu-header small text-uppercase">
+                        <span className="menu-header-text">Misc</span>
+                    </li>
+                    <MenuItem entryName="Support" entryURL="/support" entryIcon="bx-support" />
+                    <MenuItem entryName="Privacy Policy" entryURL="/privacy-policy" entryIcon="bx-file" />
 
-                        
-                        {/* Account */}
-                        <li className="menu-header small text-uppercase">
-                            <span className="menu-header-text">Account</span>
-                        </li>
-                        <MenuItem entryName="Notifications" entryURL="/notifications" entryIcon="bx-bell" />
-                        <MenuItem entryName="Chat" entryURL="/chat" entryIcon="bx-message-rounded-dots" />
+                </ul>
 
-                        {/* Misc */}
-                        <li className="menu-header small text-uppercase">
-                            <span className="menu-header-text">Misc</span>
-                        </li>
-                        <MenuItem entryName="Support" entryURL="/support" entryIcon="bx-support" />
-                        <MenuItem entryName="Privacy Policy" entryURL="/privacy-policy" entryIcon="bx-file" />
+            </aside>
+        </>
+    );
 
-                    </ul>
-
-                </aside>
-            </>
-        );
-    }
 }
  
 export default Sidebar;
--- Hillpad_staff/src/components/course/ListCourseActions.jsx
+++ Hillpad_staff/src/components/course/ListCourseActions.jsx
@@ -1,10 +1,190 @@
+import { useEffect, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+
+import useAuth from '../../hooks/useAuth';
+import courseService from '../../services/api/courseService';
+import Paginator from "../common/Paginator";
+import config from "../../config.json";
 
 
 const ListCourseActions = () => {
     
-    return ( 
+    const [courses, setCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [dataCount, setDataCount] = useState(0);
+    const [pages, setPages] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const pageSize = config.pageSize;
+
+    let location = useLocation();
+    let navigate = useNavigate();
+    let auth = useAuth();
+
+    const statusClass = {
+        "PUBLISHED": "bg-label-success",
+        "APPROVED": "bg-label-info",
+        "REJECTED": "bg-label-danger",
+        "REVIEW": "bg-label-warning",
+        "SAVED": "bg-label-secondary"
+    }
+
+    useEffect(() => {
+        async function fetchCourses() {
+            try {
+                setLoading(true);
+                const pageQuery = `page=${currentPage}`;
+                const response = await courseService.getCourseDrafts(pageQuery);
+                if (response.status === 200) {
+                    setDataCount(response.data.count);
+                    setPages(Math.ceil(dataCount / pageSize));
+                    setCourses(response.data.results);
+                }
+            } catch (ex) {
+                if (ex.response.status === 401) {
+                    navigate("/login", {
+                        state: { from: location },
+                        replace: true
+                    });
+                }
+            }
+            setLoading(false);
+        }
+        fetchCourses();
+    }, [currentPage, dataCount, location, navigate, pageSize]);
+
+    function renderCourses() {
+        if (loading) {
+            return (
+                <tr>
+                    <td className="text-center">
+                        <div className="mx-4 my-3 spinner-border text-warning" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </td>
+                    <td className="text-center">
+                        <div className="mx-4 my-3 spinner-border text-warning" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </td>
+                    <td className="text-center">
+                        <div className="mx-4 my-3 spinner-border text-warning" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </td>
+                    <td className="text-center">
+                        <div className="mx-4 my-3 spinner-border text-warning" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </td>
+                </tr>
+            );
+        }
+        else if (courses.length === 0 && !loading) {
+            return (
+                <tr>
+                    <td>
+                        <h5 className="mx-4 my-3 text-danger">No courses.</h5>
+                    </td>
+                </tr>
+            );
+        } else {
+            return (
+                <>
+                    {courses.map(course => (
+                        <tr key={course.id}>
+                            <td>
+                                <i className="fab fa-angular fa-lg text-danger me-3"></i>
+                                <strong>{course.name}</strong>
+                            </td>
+                            <td>{course.school ? course.school.name : "-"}</td>
+                            <td>
+                                {(course.degree_type && "short_name" in course.degree_type) ? course.degree_type.short_name : "-"}
+                            </td>
+                            <td>
+                                <span className={`badge ${statusClass[course.status]} me-1`}>{course.status}</span>
+                            </td>
+                            <td>
+                            <div className="dropdown">
+                                <button type="button" className="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                                    <i className="bx bx-dots-vertical-rounded"></i>
+                                </button>
+                                <div className="dropdown-menu">
+                                    <Link className="dropdown-item" to={`edit/${course.id}`}>
+                                        <i className="bx bx-edit-alt me-1"></i>
+                                        Edit
+                                    </Link>
+                                    <Link className="dropdown-item" to="https://hillpad.vercel.app">
+                                        <i className="bx bx-window me-1"></i>
+                                        View live
+                                    </Link>
+                                </div>
+                            </div>
+                            </td>
+                        </tr>
+                    
+                    ))}
+                </>
+            );
+        }
+    }
+
+    return (
         <>
-        
+            <div className="container-xxl flex-grow-1 container-p-y">
+                <div className="d-flex justify-content-between align-items-center">
+                    <h4 className="fw-bold py-3 mb-4">
+                        Courses
+                    </h4>
+                    {   
+                        auth.user &&
+                        auth.user.role === "SPECIALIST" &&
+                        <Link to="create">
+                            <button type="button" className="btn btn-secondary mb-4">
+                                <span className="tf-icons bx bx-plus"></span>&nbsp; Add Course
+                            </button>
+                        </Link>
+                    }
+                </div>
+
+                <div className="card">
+                    <div className="mt-4">
+                        <form action="" method="GET">
+                            <div className="mb-3 px-4 row">
+                                <div className="col-md-4">
+                                    <input className="form-control" type="search" placeholder="Search..." id="html5-search-input" />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div className="table-responsive text-nowrap">
+                        <table className="table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Course Name</th>
+                                    <th>School</th>
+                                    <th>Degree</th>
+                                    <th>Status</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody className="table-border-bottom-0">
+
+                                {renderCourses()}
+
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <Paginator
+                        pages={pages}
+                        currentPage={currentPage}
+                        setCurrentPage={setCurrentPage}
+                    />
+                </div>
+
+            </div>
         </>
     );
 }
