e16eb384473dd4527e2ec59532007a64115b0d67
Martins Anerua <aneruamartins@gmail.com>
2023-08-10 16:22:37 +0100

    fix: back button on school review form

--- Hillpad_staff/src/components/school/ReviewSchool.jsx
+++ Hillpad_staff/src/components/school/ReviewSchool.jsx
@@ -12,7 +12,7 @@ const ReviewSchool = () => {
             <div className="container-xxl flex-grow-1 container-p-y">
                 <div className="d-flex justify-content-between align-items-center">
                     <h4 className="fw-bold py-3 mb-4">Review School</h4>
-                    <Link to="/course/reviews">
+                    <Link to="/school/reviews">
                         <button
                             type="button"
                             className="btn btn-secondary mb-4"
